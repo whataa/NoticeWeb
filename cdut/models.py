9698d9ca29cf5961faba357ce8ab5efa620eaf5b
a598235157 <y837979117@gmail.com>
2016-05-06 19:20:57 +0800

    2016/5/6，信息列表增加字段img，修改type和source的类型

--- cdut/models.py
+++ cdut/models.py
@@ -1,7 +1,10 @@
+import json
+
+import re
 from django.db import models
 
-
 # 用户表
+from django.db.models.query_utils import Q
 from django.utils import timezone
 
 from utils.util import json_serial
@@ -15,6 +18,7 @@ class User(models.Model):
     join_time = models.DateTimeField(auto_now_add=True)
     last_visit_time = models.DateTimeField(auto_now=True)
     associate_article_ids = models.TextField(blank=True, unique=False)
+
     def __str__(self):
         return self.device_id
 
@@ -24,11 +28,13 @@ class Comment(models.Model):
     comment_id = models.AutoField(primary_key=True)
     article = models.ForeignKey('Article', unique=False)
     user = models.ForeignKey('User')
-    datetime = models.DateTimeField(unique=False,auto_now_add=True)
+    datetime = models.DateTimeField(unique=False, auto_now_add=True)
     tool = models.CharField(blank=True, unique=False, max_length=16)
     message = models.TextField(unique=False)
+
     def __str__(self):
         return self.message
+
     def toJson(self):
         item = {}
         item['id'] = self.comment_id
@@ -39,20 +45,23 @@ class Comment(models.Model):
         item['message'] = self.message
         return item
 
+
 # 访问次数表
 class VisitNum(models.Model):
     visitnum_id = models.AutoField(primary_key=True)
     article = models.ForeignKey('Article')
     pv = models.IntegerField(default=0, unique=False)
     uv = models.IntegerField(default=0, unique=False)
+
     def __str__(self):
-        return ''+self.visitnum_id
+        return '' + self.visitnum_id
 
 
 # 文章类型表
 class Type(models.Model):
     type_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=8)
+
     def __str__(self):
         return self.name
 
@@ -62,6 +71,7 @@ class Source(models.Model):
     source_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=16)
     tag = models.CharField(max_length=8)
+
     def __str__(self):
         return self.name
 
@@ -73,8 +83,10 @@ class File(models.Model):
     url = models.URLField()
     name = models.CharField(blank=True, unique=False, max_length=64)
     type = models.CharField(unique=False, max_length=8)
+
     def __str__(self):
         return self.name
+
     def toJson(self):
         item = {}
         item['id'] = self.file_id
@@ -92,19 +104,28 @@ class Article(models.Model):
     title = models.TextField()
     origin_url = models.URLField()
     author = models.CharField(blank=True, unique=False, max_length=32)
-    addtime = models.DateTimeField(unique=False,auto_now_add=True)
+    addtime = models.DateTimeField(unique=False, auto_now_add=True)
+
     def __str__(self):
         return self.title
+
     def toJson(self):
         _item = {}
         _item['id'] = self.article_id
         _item['title'] = self.title
         _item['author'] = self.author
         _item['url'] = self.origin_url
-        from utils.util import json_serial
         _item['time'] = json_serial(self.addtime)
-        _item['source'] = self.source_id
-        _item['type'] = self.type_id
+        _item['source'] = Source.objects.get(source_id=self.source_id).name
+        _item['type'] = Type.objects.get(type_id=self.type_id).name
+        try:
+            content = Content.objects.get(article_id=self.article_id)
+            pattern = re.compile(r'{"img": "(.*?)"}')
+            urls = re.findall(pattern,content.content)
+            if urls:
+                _item['img'] = urls[0]
+        except Content.DoesNotExist:
+            pass
         return _item
 
 
@@ -114,10 +135,12 @@ class Content(models.Model):
     article = models.ForeignKey('Article')
     content = models.TextField(blank=True)
     datetime = models.DateTimeField(default=timezone.now)
+
     def __str__(self):
         return self.content
+
     def toJson(self):
-        item ={}
+        item = {}
         item['id'] = self.content_id
         item['content'] = self.content
         item['dateTime'] = json_serial(self.datetime)
